82078b0b4877bac14895ee72b7a84fa7cdb29f70
DID-FIELD <1516675264@qq.com>
2025-09-27 15:29:23 +0800

    测试

---
+++ Untitled-1.py
@@ -0,0 +1,17 @@
+# %%
+print ("hello jupyter")
+
+# %%
+import numpy as np # type: ignore
+import matplotlib.pyplot as plt # type: ignore
+x = np.linspace(0.000000001, 5, 100)
+y = np.log(x )
+plt.plot(x, y)
+plt.show()
+
+
+
+# %%
+
+
+
